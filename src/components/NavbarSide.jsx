d69a2e6c0257aa002650cd71ddc4dd11df202a18
ufshoes4smiles <114452707+ufshoes4smiles@users.noreply.github.com>
2023-02-22 21:13:40 -0500

    Update NavbarSide.jsx

--- src/components/NavbarSide.jsx
+++ src/components/NavbarSide.jsx
@@ -120,7 +120,7 @@ const NavbarSide = () => {
   return (
     // lg screen classes: md:rounded-none md:mt-0  border-[5px] border-orange-600
     <div
-      className="fixed z-front bg-[#f7aa80] shadow-lg  h-[80px] flex justify-between w-screen items-center px-2 pr-4 
+      className="fixed z-front bg-[#000000] shadow-lg  h-[80px] flex justify-between w-screen items-center px-2 pr-4 
     text-blue-600 font-poppins "
     >
       <img
@@ -143,7 +143,7 @@ const NavbarSide = () => {
             >
               <li
                 key={id}
-                className="p-4 hover:text-[#ff3333] lg:text-[18px]  md:text-sm font-bold duration-300 link link-underline link-underline-black"
+                className="p-4 hover:text-blue-200 lg:text-[18px]  md:text-sm font-bold duration-300 link link-underline link-underline-black"
               >
                 {title}
               </li>
